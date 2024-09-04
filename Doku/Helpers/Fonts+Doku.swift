7dd6a817caf6d401ec6aedd304d1e52760ba3496
ege <egecam000@gmail.com>
2024-09-04 19:12:43 +0300

    add custom fonts

--- Doku/Helpers/Fonts+Doku.swift
+++ Doku/Helpers/Fonts+Doku.swift
@@ -6,3 +6,43 @@
 //
 
 import Foundation
+import SwiftUI
+import VFont
+
+extension Font {
+    static func vollkorn(
+        size: CGFloat,
+        width: CGFloat = 0,
+        weight: CGFloat = 0) -> Font {
+        return .vFont("Vollkorn", size: size, axes: [
+            2003265652: weight
+        ])
+    }
+    
+    static func literata(
+        size: CGFloat,
+        width: CGFloat = 0,
+        weight: CGFloat = 0) -> Font {
+            return .vFont("Literata", size: size, axes: [
+                2003265652: weight
+            ])
+        }
+    
+    static func raleway(
+        size: CGFloat,
+        width: CGFloat = 0,
+        weight: CGFloat = 0) -> Font {
+            return .vFont("Raleway", size: size, axes: [
+                2003265652: weight
+            ])
+        }
+    
+    static func inter(
+        size: CGFloat,
+        width: CGFloat = 0,
+        weight: CGFloat = 0) -> Font {
+            return .vFont("Inter", size: size, axes: [
+                2003265652: weight
+            ])
+        }
+}
