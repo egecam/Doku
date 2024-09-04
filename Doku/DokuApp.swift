ed72a98662f2f40deeaba3e11d58c45d0389a6b3
ege <egecam000@gmail.com>
2024-09-04 19:22:30 +0300

    implement in app notifications

--- Doku/DokuApp.swift
+++ Doku/DokuApp.swift
@@ -16,10 +16,54 @@ class AppDelegate: NSObject, UIApplicationDelegate {
         
         return true
     }
+    
+    func application(_ application: UIApplication,
+                     configurationForConnecting connectingSceneSession: UISceneSession,
+                     options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+        let configuration = UISceneConfiguration(name: nil, sessionRole: connectingSceneSession.role)
+        if connectingSceneSession.role == .windowApplication {
+            configuration.delegateClass = SceneDelegate.self
+        }
+        return configuration
+    }
 }
 
+final class SceneDelegate: NSObject, UIWindowSceneDelegate {
+    
+    var secondaryWindow: UIWindow?
+    
+    func scene(_ scene: UIScene,
+               willConnectTo session: UISceneSession,
+               options connectionOptions: UIScene.ConnectionOptions) {
+        if let windowScene = scene as? UIWindowScene {
+            setupSecondaryOverlayWindow(in: windowScene)
         }
-    }()
+    }
+    
+    func setupSecondaryOverlayWindow(in scene: UIWindowScene) {
+        let secondaryViewController = UIHostingController(
+            rootView:
+                EmptyView()
+                .frame(maxWidth: .infinity, maxHeight: .infinity)
+                .modifier(InAppNotificationViewModifier())
+        )
+        secondaryViewController.view.backgroundColor = .clear
+        let secondaryWindow = PassThroughWindow(windowScene: scene)
+        secondaryWindow.rootViewController = secondaryViewController
+        secondaryWindow.isHidden = false
+        self.secondaryWindow = secondaryWindow
+    }
+}
+
+class PassThroughWindow: UIWindow {
+    override func hitTest(_ point: CGPoint,
+                          with event: UIEvent?) -> UIView? {
+        guard let hitView = super.hitTest(point, with: event)
+        else { return nil }
+        
+        return rootViewController?.view == hitView ? nil : hitView
+    }
+}
 
 
 
@@ -28,9 +72,14 @@ struct DokuApp: App {
     @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
     @Environment(\.scenePhase) private var scenePhase
     
+    init() {
+        loadRocketSimConnect()
+    }
+    
     var body: some Scene {
         WindowGroup {
             RootView()
+                .modifier(InAppNotificationViewModifier())
         }
         .environment(\.colorScheme, .light)
         .onChange(of: scenePhase) { oldPhase, newPhase in
@@ -101,3 +150,14 @@ class SharedContentHandler {
         NotificationManager.shared.showNotification(type: .welcomeBack, content: content, showFireworks: true)
     }
 }
+
+private func loadRocketSimConnect() {
+#if DEBUG
+    guard (Bundle(path: "/Applications/RocketSim.app/Contents/Frameworks/RocketSimConnectLinker.nocache.framework")?.load() == true) else {
+        print("Failed to load linker framework")
+        return
+    }
+    print("RocketSim Connect successfully linked")
+#endif
+}
+
