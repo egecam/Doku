67c7addb17969cde0588ac45f37e7452d2107f85
ege <egecam000@gmail.com>
2024-09-04 19:21:25 +0300

    polish EntriesGridView with new functionalities

--- Doku/View/EntriesGridView.swift
+++ Doku/View/EntriesGridView.swift
@@ -6,42 +6,177 @@
 //
 
 import SwiftUI
-import LinkPresentation
 
 struct EntriesGridView: View {
-    @State private var linkPreview: LPLinkMetadata?
+    @Environment(\.colorScheme) var colorScheme
+    @ObservedObject var viewModel: HomeViewModel
+    
+    @State private var entryFavouriteStatus: Bool = false
+    @Binding var selectedTag: String?
+    
+    @State private var isCardExpanded: Bool = false
+    @State private var selectedEntry: DokuEntry?
+    @State private var selectedEntryLocation: UnitPoint?
+    
     let entries: [DokuEntry]
-    let columns = Array(repeating: GridItem(.flexible(), spacing: 18), count: 1)
+    let tags: [String]
+    
+    private let impactMed = UIImpactFeedbackGenerator(style: .medium)
+    
+    var filteredEntries: [DokuEntry] {
+        selectedTag.map { tag in
+            entries.filter { $0.tags.contains(tag) }
+        } ?? entries
+    }
     
     var body: some View {
-        ScrollView {
-            LazyVGrid(columns: columns, spacing: 18) {
-                Section {
-                    ForEach(entries) { entry in
-                        NavigationLink(destination: {
-                            // EntryDetailView(entry: entry)
-                        }) {
-                            if entry.contentType == .article || entry.contentType == .tweet || entry.contentType == .image {
-                                EntryCardView(title: entry.title, createdAt: entry.createdAt.formatted(), contentType: entry.contentType, url: entry.url!, passage: "", tags: entry.tags)
-                            } else if entry.contentType == .passage {
-                                EntryCardView(title: entry.title, createdAt: entry.createdAt.formatted(), contentType: entry.contentType, url: entry.url!, passage: entry.content, tags: entry.tags)
-                            }
-                        }
+        ZStack {
+            mainContent
+                .blur(radius: isCardExpanded ? 5 : 0)
+                .allowsHitTesting(!isCardExpanded)
+            
+            if isCardExpanded {
+                Color.black.opacity(0.5)
+                    .ignoresSafeArea()
+                    .onTapGesture {
+                            isCardExpanded = false
+                            selectedEntry = nil
+                        
                     }
-                } header: {
-                    Text("Recently Saved")
-                        .font(.title2.bold())
-                        .frame(maxWidth: .infinity, alignment: .leading)
-                }
                 
+                if let selectedEntry = selectedEntry {
+                    withAnimation(.easeIn(duration: 3)) {
+                        EntryDetailView(entry: selectedEntry)
+                            .transition(.opacity.combined(with: .scale))
+                            .zIndex(1)
+                    }
+                }
+            }
+        }
+        .animation(.spring(), value: isCardExpanded)
+    }
+    private var mainContent: some View {
+        ScrollView {
+            VStack(alignment: .leading, spacing: 20) {
+                headerView
+                tagScrollView
+                entriesGridView
             }
             .padding()
         }
-        .scrollTargetLayout()
+        .background(colorScheme == .light ? .alabaster : .jet)
         .scrollIndicators(.hidden)
-        .scrollTargetBehavior(.viewAligned)
-        .scrollBounceBehavior(.always)
-        
+    }
+    
+    private var headerView: some View {
+        HStack(alignment: .center) {
+            Text("Recently Saved")
+                .font(.vollkorn(size: 32, weight: 600))
+                .foregroundStyle(colorScheme == .light ? .jet : .alabaster)
+            
+            Spacer()
+        }
+    }
+    
+    private var tagScrollView: some View {
+        ScrollView(.horizontal, showsIndicators: false) {
+            HStack {
+                ForEach(tags, id: \.self) { tag in
+                    TagButton(tag: tag, isSelected: selectedTag == tag) {
+                        withAnimation {
+                            selectedTag = selectedTag == tag ? nil : tag
+                        }
+                    }
+                }
+            }
+        }
+    }
+    
+    private var entriesGridView: some View {
+        LazyVGrid(columns: [GridItem(.flexible()), GridItem(.flexible())], spacing: 18) {
+            ForEach(filteredEntries) { entry in
+                EntryCardView(entry: entry)
+                    .transition(.opacity.combined(with: .scale))
+                    .onTapGesture { location in
+                        selectedEntryLocation?.x = location.x
+                        selectedEntryLocation?.y = location.y
+                        selectedEntry = entry
+                        withAnimation(.spring()) {
+                            isCardExpanded = true
+                        }
+                    }
+                    .contextMenu {
+                        contextMenuButtons(for: entry)
+                    }
+            }
+        }
+    }
+    
+    private func contextMenuButtons(for entry: DokuEntry) -> some View {
+        Group {
+            if entry.tags.contains("favourite") {
+                Button("Remove from favourites", systemImage: "heart.fill") {
+                    updateFavouriteStatus(for: entry, isFavourite: false)
+                }
+            } else {
+                Button("Add to favourites", systemImage: "heart") {
+                    updateFavouriteStatus(for: entry, isFavourite: true)
+                }
+            }
+            
+            Button(role: .destructive) {
+                deleteEntry(entry)
+            } label: {
+                Label("Delete", systemImage: "trash")
+            }
+        }
+    }
+    
+    private func updateFavouriteStatus(for entry: DokuEntry, isFavourite: Bool) {
+        Task {
+            await viewModel.updateEntryFavouriteStatus(entryID: entry.id!, entryHasFavouriteTag: !isFavourite)
+            NotificationManager.shared.showNotification(
+                type: isFavourite ? .favourite : .unfavourite,
+                content: "Entry is \(isFavourite ? "added to" : "removed from") your favourites."
+            )
+            impactMed.impactOccurred()
+        }
+    }
+    
+    private func deleteEntry(_ entry: DokuEntry) {
+        Task {
+            await viewModel.deleteEntry(entryID: entry.id!)
+            NotificationManager.shared.showNotification(type: .delete, content: "Entry is removed from your Doku.")
+            viewModel.fetchTags()
+            impactMed.impactOccurred()
+        }
+    }
+    
+}
+
+struct TagButton: View {
+    let tag: String
+    let isSelected: Bool
+    let action: () -> Void
+    
+    @Environment(\.colorScheme) var colorScheme
+    
+    var body: some View {
+        Button(action: action) {
+            if tag == "favourite" {
+                Label("Favourites", systemImage: "heart.fill")
+            } else {
+                Text(tag)
+            }
+        }
+        .padding()
+        .frame(height: 30)
+        .foregroundStyle(isSelected ? .alabaster : (colorScheme == .light ? .jet : .gray))
+        .background(Capsule().fill(isSelected ? Color.coral : (colorScheme == .light ? Color.davysGray.opacity(0.3) : Color.davysGray)))
+        .animation(.easeInOut, value: isSelected)
     }
 }
 
+#Preview {
+    HomeView()
+}
