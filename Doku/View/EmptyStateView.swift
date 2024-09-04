e43056e583db24f6b131f88b40b1e944111c3486
ege <egecam000@gmail.com>
2024-09-04 19:18:07 +0300

    add new setting options
    ...also some UI updates

--- Doku/View/EmptyStateView.swift
+++ Doku/View/EmptyStateView.swift
@@ -8,23 +8,37 @@
 import SwiftUI
 
 struct EmptyStateView: View {
+    @Environment(\.colorScheme) var colorScheme
+    
     var body: some View {
-        VStack {
-            Text("Your Doku is empty.")
-                .font(.title.bold())
-                .fixedSize(horizontal: false, vertical: true)
+        ZStack {
+            colorScheme == .light ? Color.alabaster.ignoresSafeArea() : Color.jet
+                .ignoresSafeArea()
             
-            Text("Saved Dokus will be shown here")
-                .font(.subheadline)
-            
-            Text("Share anything on any app with Doku.")
-                .font(.headline)
+            VStack(alignment: .center, spacing: 10) {
+                Text("Your Doku is empty.")
+                    .font(.vollkorn(size: 24, weight: 600))
+                    .fixedSize(horizontal: false, vertical: true)
+                
+                Text("Saved things will be shown here.")
+                    .font(.raleway(size: 16, weight: 500))
+                
+                HStack(spacing: 16) {
+                    Image(systemName: "info.bubble.fill")
+                        .imageScale(.large)
+                    
+                    Text("Share a link, text or tweet with Doku.")
+                        .font(.literata(size: 18, weight: 500))
+                        .multilineTextAlignment(.leading)
+                }
                 .padding(.top, 20)
-                .multilineTextAlignment(.center)
-            
+                .symbolEffect(.pulse)
+            }
+            .padding()
+            .frame(alignment: .center)
         }
-        .padding()
-        .frame(alignment: .center)
+        .foregroundStyle(colorScheme == .light ? .jet : .alabaster)
+        .ignoresSafeArea()
     }
 }
 
